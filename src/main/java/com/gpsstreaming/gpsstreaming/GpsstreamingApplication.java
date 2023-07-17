709a9746721a180b2ac055485bfc025b956855eb
Dharecha Rameshkumar Devshibhai <rameshdharecha91@gmail.com>
2023-07-17 20:25:17 +0530

    Initial project GPS Streaming

---
+++ src/main/java/com/gpsstreaming/gpsstreaming/GpsstreamingApplication.java
@@ -0,0 +1,13 @@
+package com.gpsstreaming.gpsstreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsstreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsstreamingApplication.class, args);
+	}
+
+}
